c5b7add41a74ea42135ddd5b4672f5c6486854f0
hellen228 <hellen.juandedios@gmail.com>
2026-07-21 11:01:06 -0500

    cambio de nombre

---
+++ src/main/java/com/essalud/EsSaludApplication.java
@@ -0,0 +1,11 @@
+package com.essalud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsSaludApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EsSaludApplication.class, args);
+    }
+}
